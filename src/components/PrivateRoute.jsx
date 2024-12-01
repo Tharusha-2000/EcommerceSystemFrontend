73d36dcf3c10c44fa49ba934ae3d2c4e67a59714
jinu <hasarijithmi@gmail.com>
2024-12-01 16:55:55 +0530

    commented route

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,21 +1,32 @@
+// import { Navigate } from "react-router-dom";
+// import { useSelector } from "react-redux";
+
+// const PrivateRoute = ({ children }) => {
+//   const { currentUser } = useSelector((state) => state.user);
+//   console.log(currentUser);
+//   if (!currentUser) {
+//     return <Navigate to="/" />;
+//   }
+
+//   // Check if the user is logged in and has the "admin" role
+//   if (currentUser.role === "Admin") {
+//     return children;
+//   } else {
+//     // Redirect customer to their home page (or any other page)
+//     return <Navigate to="/" />;
+//   }
+
+// };
+
+// export default PrivateRoute;
+
 import { Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 const PrivateRoute = ({ children }) => {
   const { currentUser } = useSelector((state) => state.user);
-  console.log(currentUser);
-  if (!currentUser) {
-    return <Navigate to="/" />;
-  }
 
-  // Check if the user is logged in and has the "admin" role
-  if (currentUser.role === "Admin") {
-    return children;
-  } else {
-    // Redirect customer to their home page (or any other page)
-    return <Navigate to="/" />;
-  }
-
+  return true ? children : <Navigate to="/" />; //currentUser && currentUser.role === "admin"
 };
 
 export default PrivateRoute;
